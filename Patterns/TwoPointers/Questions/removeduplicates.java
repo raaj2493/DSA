1cd3c887cc69d90601232ea0b0b1b1b2851cd139
Raaj Giri <raj.ash2493@gmail.com>
2026-01-05 22:30:33 +0530

    Two Pointers

---
+++ Patterns/TwoPointers/Questions/removeduplicates.java
@@ -0,0 +1,21 @@
+package Patterns.TwoPointers.Questions;
+
+public class removeduplicates {
+    public static void main(String[] args) {
+        
+    }
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0; // index of last unique element
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1; // number of unique elements
+    }
+}
